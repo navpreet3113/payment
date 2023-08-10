4fd1000e58312014a69b942e9bdbed72e1f136dd
Alok Kumar <59285395+alokkr016@users.noreply.github.com>
2023-08-10 17:56:42 +0530

    Few changes

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,27 +1,55 @@
-import React from "react";
-import "./login.css"; // Make sure to create a separate CSS file for the styles
+// login.css and paymentReceipt.css remain the same
+
+import React, { useState } from "react";
+import "./login.css";
 import PaymentReceipt from "./PaymentReceipt";
 
 const LoginPage = () => {
-  return (
-    <div className="main">
-      <div className="container">
-        <div className="login">
-          <h3 className="title">User Login</h3>
-          <div className="text-input">
-            <i className="ri-user-fill"></i>
-            <input id="username" type="text" placeholder="Username" />
-          </div>
-          <div className="text-input">
-            <i className="ri-lock-fill"></i>
-            <input id="password" type="password" placeholder="Password" />
-          </div>
-          <button className="login-btn">LOGIN</button>
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
 
-          <a href="#" className="forgot">
-            Forgot Username/Password?
-          </a>
+  const handleLogin = () => {
+    if (username === "root" && password === "root") {
+      setIsLoggedIn(true);
+    }
+  };
+
+  if (isLoggedIn) {
+    return <PaymentReceipt />;
+  }
+
+  return (
+    <div className="container">
+      <div className="login">
+        <h3 className="title">User Login</h3>
+        <div className="text-input">
+          <i className="ri-user-fill"></i>
+          <input
+            id="username"
+            type="text"
+            placeholder="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
         </div>
+        <div className="text-input">
+          <i className="ri-lock-fill"></i>
+          <input
+            id="password"
+            type="password"
+            placeholder="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button className="login-btn" onClick={handleLogin}>
+          LOGIN
+        </button>
+
+        <a href="#" className="forgot">
+          Forgot Username/Password?
+        </a>
       </div>
     </div>
   );
